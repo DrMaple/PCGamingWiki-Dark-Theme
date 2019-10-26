4de23385d3d94ef3c44528b8ece55bf84c54d566
DrMaple <mrmaple24@gmail.com>
2019-10-26 18:30:16 -0400

    Update PCGamingWikiDarkTheme.js
    Updates

--- PCGamingWikiDarkTheme.js
+++ PCGamingWikiDarkTheme.js
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  Dark Mode for PcGamingWiki
 // @author       Maple
-// @match        https://www.pcgamingwiki.com/*
+// @match        *.pcgamingwiki.com/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/DrMaple/PCGamingWiki-Dark-Theme/master/PCGamingWikiDarkTheme.js
 // @downloadURL  https://raw.githubusercontent.com/DrMaple/PCGamingWiki-Dark-Theme/master/PCGamingWikiDarkTheme.js
@@ -11,19 +11,19 @@
 
 (function (window, undefined) {
     var css = [
-        'body, tr:nth-child(odd), #toc, .toc, html, .article-image-text, #preferences, .page-Home .home-card, .project-intro, .editing-guide-list-item, .page-Home .home-subheader {',
-		'	background-color: #292a2d !important',
+        'body, tr:nth-child(odd), #toc, .toc, html, .article-image-text, #preferences, .page-Home .home-card, .project-intro, .editing-guide-list-item, .page-Home .home-subheader, .ipsPad, .ipsApp ul.ipsPad, .ipsApp ol.ipsPad, .ipsWidget.ipsWidget_horizontal:not( .ipsWidgetHide ) + .ipsWidget {',
+		'	background-color: #292929 !important',
 		'}',
-		'#main-content, tr:nth-child(even), #catlinks, .page-Home .table-assignment-body-row:nth-of-type(2n) .table-assignment-body-game, .page-Home .table-assignment-body-row:nth-of-type(2n) .table-assignment-body-status {',
+		'#main-content, tr:nth-child(even), #catlinks, .page-Home .table-assignment-body-row:nth-of-type(2n) .table-assignment-body-game, .page-Home .table-assignment-body-row:nth-of-type(2n) .table-assignment-body-status, .ipsDataList_large .ipsDataItem_main, .ipsDataList_large .ipsDataItem_stats, .ipsDataList_large .ipsDataItem_lastPoster, .ipsDataList_large .ipsDataItem_generic, ipsDataItem_category, .ipsDataItem_main, .ipsDataItem_stats, .ipsDataItem_lastPoster, .ipsDataItem_generic, .ipsDataItem_modCheck, .ipsDataItem_icon, .ipsWidget.ipsWidget_vertical .ipsWidget_title, .ipsWidget.ipsWidget_horizontal .ipsWidget_title, #ipsLayout_header nav, .ipsNavBar_primary:not( .ipsNavBar_noSubBars ) > ul:before, .ipsPad_half, .ipsApp ul.ipsPad_half, .ipsApp ol.ipsPad_half, .ipsNavBar_secondary, #elSearchExpanded, html[dir="ltr"] .ipsSideMenu_withChecks .ipsSideMenu_item a, html[dir="ltr"] .ipsSideMenu_withChecks a.ipsSideMenu_item, html[dir="ltr"] .ipsSideMenu_withChecks span.ipsSideMenu_item, html[dir="ltr"] .ipsSideMenu_withRadios .ipsSideMenu_item a, html[dir="ltr"] .ipsSideMenu_withRadios a.ipsSideMenu_item, html[dir="ltr"] .ipsSideMenu_withRadios span.ipsSideMenu_item {',
 		'	background-color: #252525 !important',
 		'}',
-        '.pcgwikitable, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, body, .template-infobox, code, #preferences legend, .htmlform-tip, .page-Home .home-card, .project-intro, .page-Home .home-subheader, table.wikitable {',
+        '.pcgwikitable, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, body, .template-infobox, code, #preferences legend, .htmlform-tip, .page-Home .home-card, .project-intro, .page-Home .home-subheader, table.wikitable, .ipsType_richText, .ipsBreadcrumb > ul > li > a {',
 		'	color: #ffffff !important',
 		'}',
         '#toc, .toc, code {',
 		'	border: 1px solid #ffffff !important',
 		'}',
-        '.pcgw-footer, code {',
+        '.pcgw-footer, code, .ipsTabs.ipsTabs_stretch.ipsTabs_small .ipsTabs_item {',
 		'	background-color: #333333 !important',
 		'}',
         'a {',
@@ -32,8 +32,17 @@
         'a:visited {',
 		'	color: #5d8496 !important',
 		'}',
-		'kbd {',
+		'kbd, .ipsItemStatus.ipsItemStatus_large {',
 		'	color: black !important',
+		'}',
+        '#ipsLayout_header header {',
+		'	background-color: #485873 !important',
+		'}',
+        '.ipsNavBar_primary > ul > li > a {',
+		'	background: #232c48 !important',
+		'}',
+        '#elSearchExpanded .ipsMenu_title {',
+		'	background: #292929 !important',
 		'}'
 
 
